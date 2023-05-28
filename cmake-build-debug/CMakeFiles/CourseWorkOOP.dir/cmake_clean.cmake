00d67cd12fb85121cdca1c6716add518dac98931
FIJIEKCFIHDP <shamraev.alexandr@gmail.com>
2023-05-28 19:54:30 +0300

    Cmake setup

---
+++ cmake-build-debug/CMakeFiles/CourseWorkOOP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CourseWorkOOP.dir/main.cpp.obj"
+  "CMakeFiles/CourseWorkOOP.dir/main.cpp.obj.d"
+  "Y:/work/shuhov/oop/Pikitos/CurseWorkOOP/bin/debug/CourseWorkOOP_debug.exe"
+  "Y:/work/shuhov/oop/Pikitos/CurseWorkOOP/bin/debug/CourseWorkOOP_debug.exe.manifest"
+  "Y:/work/shuhov/oop/Pikitos/CurseWorkOOP/bin/debug/CourseWorkOOP_debug.pdb"
+  "libCourseWorkOOP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CourseWorkOOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CourseWorkOOP.dir/main.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/CourseWorkOOP.dir/main.cpp.obj: \
+ Y:\work\shuhov\oop\Pikitos\CurseWorkOOP\main.cpp

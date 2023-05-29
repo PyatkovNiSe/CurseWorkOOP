757e4eb31bcd2de99713442b602807cbd6beb87d
FIJIEKCFIHDP <shamraev.alexandr@gmail.com>
2023-05-29 23:48:44 +0300

    cleanup

--- assets/scripts/Scripts.h
+++
@@ -1,8 +0,0 @@
-#ifndef COURSEWORKOOP_ASSETS_SCRIPTS_SCRIPTS_H
-#define COURSEWORKOOP_ASSETS_SCRIPTS_SCRIPTS_H
-
-#include "gameplay/Gameplay.h"
-#include "utility/Utility.h"
-#include "ui/Ui.h"
-
-#endif //COURSEWORKOOP_ASSETS_SCRIPTS_SCRIPTS_H

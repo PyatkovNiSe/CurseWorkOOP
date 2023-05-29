b3a71a7561d612869c409cecc227f22f780a85cd
FIJIEKCFIHDP <shamraev.alexandr@gmail.com>
2023-05-29 23:42:07 +0300

    gitignore added

--- assets/scripts/gameplay/Building.h
+++ assets/scripts/gameplay/Building.h
@@ -15,48 +15,20 @@ namespace structopolis::gameplay {
 class Building {
  public:
 	utility::Signal<void(Resources)> OnProduced;
-	utility::Signal<void(Cell*)> OnBuilt;
 
  private:
 	const BuildingInfo *info_;
-	float multiplier_ = 1;
 
  public:
 	constexpr Building(const BuildingInfo *info, float multiplier)
-			: info_(info), multiplier_(multiplier) {}
+			: info_(info) {}
 
 	[[nodiscard]]
 	const BuildingInfo *GetInfo() const {
 		return info_;
 	}
 
-	[[nodiscard]]
-	float GetMultiplier() const {
-		return multiplier_;
-	}
-
-	void SetMultiplier(float multiplier) {
-		multiplier_ = multiplier;
-	}
-
-	void Build(Cell* cell) {
-		Resources production = GetMin(cell->resources, info_.);
-		if (!cell->empty || production == Resources{}) {
-			return;
-		}
-
-		production_ = production;
-		std::jthread([this]{
-			while(true) {
-				std::this_thread::sleep_for(std::chrono::duration<float>(
-						production_time_ / multiplier_));
-				OnProduced.Notify(production_);
-			}
-		}).detach();
-
-		cell->empty = false;
-		OnBuilt.Notify(cell);
-	}
+	virtual bool Build(Cell* cell) = 0;
 };
 }
 
